41791f130ad789274db6e1d0ced15f145ecf1427
gardsa <samantha.ellen.gard@gmail.com>
2016-10-11 13:08:22 +1300

    editBank functionality (not quite working - bankType issue)

--- js/dbHandling.js
+++ js/dbHandling.js
@@ -36,7 +36,47 @@ function createBank(formObj) {
 }
 
 function populateEditBank(selectedBank) {
-  console.log(selectedBank);
+  // console.log(selectedBank);
   var title = selectedBank.title;
-  document.getElementById("title").innerHTML = ("Edit > " + title);
+  document.getElementById("title").innerHTML = ("Edit / " + title);
+  document.getElementById("formTitle").value = title;
+
+  var owner = selectedBank.owner;
+  document.getElementById("formOwner").value = owner;
+
+  var planDetails = selectedBank.planDetails;
+  document.getElementById("formBankDetails").value = planDetails;
+
+  var chargeType = selectedBank.chargeType;
+  document.getElementById("formChargeType").value = chargeType;
+
+  var capacity = selectedBank.capacity;
+  document.getElementById("formCapacity").value = capacity;
+}
+
+function editBank(){
+  var formTitle = formObj.title.value.toLowerCase();
+  formTitle = formTitle.replace(/\s+/g, '');
+  var dataKey = formTitle;
+  var dbRef = firebase.database().ref('/banks/' + formBankType + '/' + formTitle);
+
+  dbRef.set({
+    title: formObj.title.value,
+    availableCharge: '0',
+    capacity: formObj.capacity.value,
+    owner: formObj.owner.value,
+    planDetails: formObj.planDetails.value,
+    chargeType: formObj.chargeType.value
+  },
+  function(){
+    document.getElementById("addBankForm").reset();
+
+    $('#addBankBtn').notify(
+      "Bank Added",
+      {
+        position:"bottom center",
+        style: "addBankSuccess",
+        className: "base"
+      });
+  });
 }
